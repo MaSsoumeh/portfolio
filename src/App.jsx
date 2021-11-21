076f68b0b27cb32443b15082a45f2a49523b52e2
MaSsoumeh <massoumehzarei@gmail.com>
2021-11-21 23:49:36 +0330

    add useState hook for burger and sidebar menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import './app.scss';
+import { useState } from 'react';
 import Topbar from './components/topbar/Topbar';
 import Intro from './components/intro/Intro';
 import Portfolio from './components/portfolio/Portfolio';
 import Works from './components/works/Works';
 import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
+import Menu from './components/menu/Menu';
 // import { routes, route } from 'react-router-dom';
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className='app'>
-      <Topbar />
-
+      <Topbar open={menuOpen} setOpen={setMenuOpen} />
+      <Menu open={menuOpen} setOpen={setMenuOpen} />
       <div className='sections'>
         <Intro />
         <Portfolio />
